94d35d0070eea220814c43827cdc64077b4f3925
vandyb369 <92529299+vandyb369@users.noreply.github.com>
2024-07-13 11:14:38 +0530

    Day1

---
+++ Day1VariablesAndDataTypes.js
@@ -0,0 +1,37 @@
+ var age = 21 
+ console.log(age)
+
+ let name1 = "vedant"
+ console.log(name1)
+
+ const isAdult = true;
+ console.log(isAdult)
+
+ const marks = 100
+ console.log(typeof marks)
+
+ const name2 = "Ram"
+ console.log(typeof name2)
+
+ const isGoodMarks = true
+ console.log(typeof isGoodMarks)
+
+ const student = {
+     name3 : "Sohan",
+     age : 22
+ }
+ console.log(typeof student)
+
+ const items = ["eggs" , "drinks" , "bread" , "candy"]
+ console.log(typeof items)
+
+ let schoolName = "MVM"
+ console.log(schoolName)
+
+ schoolName = "BVB"
+ console.log(schoolName)
+
+ const subject = "Maths"
+ subject = "science"
+
+
